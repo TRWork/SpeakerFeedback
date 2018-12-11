ae71098d5fc22892973b12ee38ee949850ac41ef
Guillermo García Subirana <guillermogarsub@gmail.com>
2018-12-11 13:36:48 +0100

    TODO 2: Created activity with layaout for room ID

---
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/RoomID.java
@@ -0,0 +1,20 @@
+package edu.upc.citm.android.speakerfeedback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RoomID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+
+        
+    }
+
+    public void OnContinue(View view) {
+    }
+}
